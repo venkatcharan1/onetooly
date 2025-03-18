4bfc4c6a9de38a04e6b2f9688d119ba8b008d371
Venkat charan <132944243+venkatcharan1@users.noreply.github.com>
2025-03-18 15:46:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -118,3 +118,10 @@ function showInstallBanner() {
 
 
 
+//pwa installation
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("✅ Service Worker Registered!"))
+    .catch((error) => console.error("❌ Service Worker Registration Failed:", error));
+}
+
